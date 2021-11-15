25dde30ca81756509a0035a238236ef8f09d9cb1
SY <oowwo@naver.com>
2021-11-16 03:32:44 +0900

    문자열 치환

--- section-1/9.A를#으로.js
+++ section-1/9.A를#으로.js
@@ -1,10 +1,14 @@
-let idx = [];
+let newString = [];
 function solution(s) {
-  let answer;
-  for (let i = 0; i < s.length; i++) {
-    answer = s.replace(/A/gi, "#");
-  }
-  return answer;
+  //정규표현식으로 풀기
+  //   let answer;
+  //   for (let i = 0; i < s.length; i++) {
+  //     answer = s.replace(/A/gi, "#");
+  //   }
+
+  //split과 map()으로 풀기
+  s = s.split("").map((item) => (item === "A" ? "#" : item));
+  return s.join("");
 }
 
 let str = "BANANA";

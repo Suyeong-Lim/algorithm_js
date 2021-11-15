d00fa33e2d18d18e78a46e0a804adbfaf8f4a57b
SY <oowwo@naver.com>
2021-11-16 04:42:41 +0900

    완전탐색.앞보다 큰수출력

--- section-1/10.문자찾기.js
+++ section-1/10.문자찾기.js
@@ -1,3 +1,4 @@
+//for-of 사용하기
 function solution(s, t) {
   let count = 0;
   for (x of s) {
@@ -8,6 +9,12 @@ function solution(s, t) {
   answer = count;
   return answer;
 }
+
+// //혹은 타겟기준으로 split 해준 후에 -1을 해줘도 된다.
+// function solution(s, t) {
+//   let answer = s.split(t);
+//   return answer.length - 1;
+// }
 
 let str = "COMPUTERPROGRAMMING";
 console.log(solution(str, "R"));

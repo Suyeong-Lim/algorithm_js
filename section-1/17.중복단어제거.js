43fe5d6b7bc4f914243f01211f528d66d34076d4
SY <oowwo@naver.com>
2021-11-25 00:34:14 +0900

    중복단어제거

--- section-1/17.중복단어제거.js
+++ section-1/17.중복단어제거.js
@@ -1,4 +1,13 @@
 function solution(s) {
+  let set = new Set();
+  let answer = [];
+
+  for (x of s) {
+    set.add(x);
+  }
+
+  answer = [...set];
+
   return answer;
 }
 let str = ["good", "time", "good", "time", "student"];

f513a706f03b7498639b2838d9bb15ad5bce3e15
SY <oowwo@naver.com>
2021-11-25 17:46:20 +0900

    완전탐색,자릿수의합

---
+++ section-4/1.자릿수의합.js
@@ -0,0 +1,26 @@
+function solution(n, arr) {
+  let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+  let sum = 0;
+  sumArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i];
+    sum = 0;
+    while (temp) {
+      sum += temp % 10;
+      temp = Math.floor(temp / 10);
+    }
+    sumArr.push(sum);
+    if (max < sum) {
+      max = sum;
+      answer = arr[i];
+    } else if (sum === max) {
+      if (arr[i] > answer) answer = arr[i];
+    }
+  }
+
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));

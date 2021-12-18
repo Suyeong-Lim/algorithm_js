21ae83415e9db14f547de6191ea428d53f49189f
SY <oowwo@naver.com>
2021-12-19 08:39:18 +0900

    아나그램

--- section-5/6.학급회장.js
+++ section-5/6.학급회장.js
@@ -1,14 +1,16 @@
 function solution(s) {
   let answer;
   let sh = new Map();
-
-  for (let x of str) {
-    if (sh.has(x)) sh.set(x, sh.get(x) + 1);
-    else sh.set(x, 1);
-  }
-  console.log(sh);
-
   let max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of s) {
+    if (sh.has(x)) {
+      sh.set(x, sh.get(x) + 1);
+    } else {
+      sh.set(x, 1);
+    }
+  }
+
   for (let [key, val] of sh) {
     if (val > max) {
       max = val;

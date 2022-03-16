4ba50eebebf6fe5e2c4d5891c825241f1ec0c99e
SY <oowwo@naver.com>
2022-03-16 23:11:02 +0900

    두개뽑아서더하기

---
+++ programers/32.두개뽑아서더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  var answer = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let numSum = numbers[i] + numbers[j];
+      if (!answer.includes(numSum)) answer.push(numSum);
+    }
+  }
+
+  return answer.sort((a, b) => a - b);
+}

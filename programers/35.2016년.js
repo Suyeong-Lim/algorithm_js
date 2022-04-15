09e9ab8e44ef932e6f706cb3fa3b58fe41d1e4c2
SY <oowwo@naver.com>
2022-04-15 19:50:39 +0900

    0415

--- programers/35.2016년.js
+++ programers/35.2016년.js
@@ -1,12 +1,45 @@
-function solution(a, b) {
+function solution(month, day) {
   let answer = 0;
 
-  const day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const monthNumberOfYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+  let pastDaySum = 0;
+
+  //for문 돌면서 누적 일 계산. 0부터 시작이니까 -1로 값 보정해줌
+  for (let i = 0; i < month - 1; i++) {
+    pastDaySum += monthNumberOfYear[i];
+  }
+  pastDaySum += day - 1;
+  console.log(pastDaySum);
+
+  switch (pastDaySum % 7) {
+    case 0:
+      answer = "FRI";
+      break;
+    case 1:
+      answer = "SAT";
+      break;
+    case 2:
+      answer = "SUN";
+      break;
+    case 3:
+      answer = "MON";
+      break;
+    case 4:
+      answer = "TUE";
+      break;
+    case 5:
+      answer = "WED";
+      break;
+    case 6:
+      answer = "THU";
+      break;
+  }
 
   return answer;
 }
 
-console.log(solution(cards));
-
 let a = 5;
 let b = 24;
+
+console.log(solution(a, b));
